0ad950426e509e4fe3a8ffdec4bfce17e7cc1b28
Avdheshupadhyay <avdheshupadhyay34@gmail.com>
2026-06-20 19:08:03 +0530

    Project Setup

---
+++ showdekhle/src/main/java/com/project/showdekhle/ShowdekhleApplication.java
@@ -0,0 +1,13 @@
+package com.project.showdekhle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowdekhleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowdekhleApplication.class, args);
+	}
+
+}
